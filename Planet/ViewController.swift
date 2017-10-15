789963a8351da5a83d5779ba01de379acd0e4483
joshmartinedevelops <joshmartine@josh.local>
2017-10-15 12:16:04 -0400

    Initial Commit

---
+++ Planet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Planet
+//
+//  Created by Josh Martine on 10/15/17.
+//  Copyright © 2017 Josh Martine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
